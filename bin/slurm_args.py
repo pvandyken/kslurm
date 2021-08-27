0b83b7ce52abbbffd7fdd822bee3956f7cfcfade
Peter Van Dyken <pvandyk2@uwo.ca>
2021-08-27 19:13:17 -0400

    Improved interface and defaults

--- bin/slurm_args.py
+++ bin/slurm_args.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import re
 from typing import Iterable, List, Tuple, Union
 
@@ -9,21 +10,24 @@ class SlurmCommand:
     JUPYTER = 'jupyter'
     ACCOUNT = 'account'
     COMMAND = 'command'
+    DIRECTORY = 'directory'
 
 
-    def __init__(self, args: List[str]):
+    def __init__(self, args: List[str], time=180, cpu=1, gpu=False, jupyter=False, account='def-lpalaniy', mem=4000):
         labels = [label_arg(a) for a in args]
         slurm_args, self._command = delineate_command(args, labels)
         labelled = zip(slurm_args, labels)
         
+        self._name = ""
         self.submit_script = [self.command]
 
-        self.time = 180
-        self.cpu = 1
-        self.gpu = False
-        self.jupyter = False
-        self.account = 'def-lpalaniy'
-        self.mem = 4000
+        self.time = time
+        self.cpu = cpu
+        self.gpu = gpu
+        self.jupyter = jupyter
+        self.account = account
+        self.mem = mem
+        self.cwd  = Path.cwd()
         [self._add_arg(*a) for a in labelled]
 
     @property
@@ -41,11 +45,16 @@ class SlurmCommand:
     def command_list(self):
         return self._command
 
-
-
     @property
     def name(self):
-        return self._command[0]
+        if not self._name:
+            return self._command[0]
+        else:
+            return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
         
     @property
     def interactive(self):
@@ -56,11 +65,12 @@ class SlurmCommand:
 
     @property
     def batch(self):
-        s = f"sbatch {self.slurm_args} --job-name={self.name}"
+        s = f"sbatch {self.slurm_args} --job-name={self.name} --parsable --output=\"snakemake-%j.out\""
         if self.command:
             return f"echo '{self.submit_script}' | {s}"
         else:
             raise Exception("No command given")
+
     @property
     def submit_script(self):
         return self._submit_script
@@ -80,6 +90,8 @@ class SlurmCommand:
             self.mem = self._to_mb(arg)
         elif label == self.JUPYTER:
             self.jupyter=True
+        elif label == self.DIRECTORY:
+            self.cwd = Path(arg).resolve()
         else:
             raise Exception("Unlabelled slurm argument")
         
@@ -117,5 +129,7 @@ def label_arg(arg):
         return SlurmCommand.MEM
     elif arg == "jupyter":
         return SlurmCommand.JUPYTER
+    elif Path(arg).exists() and Path(arg).is_dir():
+        return SlurmCommand.DIRECTORY
     else:
         return SlurmCommand.COMMAND

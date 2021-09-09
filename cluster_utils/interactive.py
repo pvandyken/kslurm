b06f7d58489d1cfd6eb80d036305da01bb18401d
Peter Van Dyken <pvandyk2@uwo.ca>
2021-09-08 20:08:06 -0400

    No longer executable scripts

--- cluster_utils/interactive.py
+++ cluster_utils/interactive.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
 import sys, subprocess
 from colorama import Fore
 from cluster_utils.slurm import SlurmCommand
+from cluster_utils.slurm import ArgList
 
 def main():
-
-    slurm = SlurmCommand(sys.argv[1:])
+    slurm = SlurmCommand(sys.argv[1:], ArgList())
     print(f"{Fore.GREEN}Running interactive session with {Fore.WHITE + slurm.slurm_args}")
     subprocess.run(slurm.interactive, shell=True)
 
--- cluster_utils/ssnake.py
+++ cluster_utils/ssnake.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import sys, subprocess
 from pathlib import Path
 from colorama import Fore, Style

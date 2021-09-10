0be95b15600361ddae4ca109117e6e2d0a268456
Peter Van Dyken <pvandyk2@uwo.ca>
2021-09-10 10:53:04 -0400

    Moved text to seperate module

--- cluster_utils/submission.py
+++ cluster_utils/submission.py
@@ -3,46 +3,31 @@ from colorama import Fore
 
 from cluster_utils.slurm import SlurmCommand
 from cluster_utils.slurm import ArgList
-
-settings_header = f"{Fore.LIGHTBLUE_EX}SETTINGS{Fore.RESET}"
-command_header = f"{Fore.LIGHTBLUE_EX}COMMAND{Fore.RESET}"
+from . import text as txt
 
 def kbatch():
     slurm = SlurmCommand(sys.argv[1:], ArgList())
     command = slurm.command if slurm.command else f"{Fore.RED}Must provide a command"
 
-    print(f"""
-    {Fore.GREEN}Scheduling Batch Command
-        {settings_header}
-            {Fore.WHITE}{slurm.slurm_args}
-        {command_header}
-            {Fore.WHITE}{command}
-    """)
+    print(txt.KBATCH_MSG.format(slurm_args=slurm.slurm_args, command=command))
     if slurm.command:
         subprocess.run(slurm.batch, shell=True)
 
 def krun():
     slurm = SlurmCommand(sys.argv[1:], ArgList())
     if slurm.command:
+        # We pre-resolve the command for full transparency
         resolved_command = subprocess\
             .run(f"echo \"{slurm.command}\"", shell=True, capture_output=True)\
             .stdout.decode()
-        print(f"""
-    {Fore.GREEN}Running job 
-        {settings_header}
-            {Fore.WHITE + slurm.slurm_args}
-        {command_header}
-            {slurm.command}""")
-        if resolved_command != slurm.command:
-            print(f"""
-            {Fore.LIGHTBLUE_EX}resolved as{Fore.RESET}
-            {resolved_command}""")
+        command_msg = "\n".join([
+            slurm.command, 
+            f"{Fore.LIGHTBLUE_EX}resolved as {Fore.RESET}", 
+            resolved_command
+        ])
+        print(txt.KRUN_CMD_MESSAGE.format(args=slurm.slurm_args, command=command_msg))
     else:
-        print(f"""
-    {Fore.GREEN}Running interactive session
-        {settings_header}
-            {Fore.WHITE + slurm.slurm_args}
-        """)
+        print(txt.INTERACTIVE_MSG.format(args=slurm.slurm_args))
 
     if not slurm.test:
         subprocess.run(slurm.run, shell=True)
@@ -50,19 +35,9 @@ def krun():
 def kalloc():
     slurm = SlurmCommand(sys.argv[1:], ArgList())
     if slurm.command:
-        print(f"""
-    {Fore.GREEN}Running job as script 
-        {settings_header}
-            {Fore.WHITE + slurm.slurm_args}
-        {command_header}
-            {slurm.command}
-        """)
+        print(txt.KALLOC_MSG.format(args=slurm.slurm_args, command=slurm.command))
     else:
-        print(f"""
-    {Fore.GREEN}Running interactive session
-        {settings_header}
-            {Fore.WHITE + slurm.slurm_args}
-        """)
+        print(txt.INTERACTIVE_MSG.format(args=slurm.slurm_args))
     if not slurm.test:
         subprocess.run(slurm.alloc, shell=True)
 
---
+++ cluster_utils/text.py
@@ -0,0 +1,33 @@
+from colorama import Fore
+
+SETTINGS_HEADER = f"{Fore.LIGHTBLUE_EX}SETTINGS{Fore.RESET}"
+COMMAND_HEADER = f"{Fore.LIGHTBLUE_EX}COMMAND{Fore.RESET}"
+
+KBATCH_MSG = f""" 
+    {Fore.GREEN}Scheduling Batch Command
+        {SETTINGS_HEADER}
+            {Fore.WHITE}{{slurm_args}}
+        {COMMAND_HEADER}
+            {Fore.WHITE}{{command}}
+"""
+
+KRUN_CMD_MESSAGE = f""" \
+    {Fore.GREEN}Running job 
+        {SETTINGS_HEADER}
+            {Fore.WHITE} {{args}}
+        {COMMAND_HEADER}
+            {{command}}"""
+
+INTERACTIVE_MSG = f"""
+    {Fore.GREEN}Running interactive session
+        {SETTINGS_HEADER}
+            {Fore.WHITE} {{args}}
+"""
+
+KALLOC_MSG = f"""
+    {Fore.GREEN}Running job as script 
+        {SETTINGS_HEADER}
+            {Fore.WHITE} {{args}}
+        {COMMAND_HEADER}
+            {{command}}
+"""

cd04c419e3fc7376ba402734087f7a84ab82bb3a
Peter Van Dyken <pvandyk2@uwo.ca>
2022-04-22 15:35:31 -0400

    Implement kpy create

--- kslurm/cli/kpy.py
+++ kslurm/cli/kpy.py
@@ -4,14 +4,16 @@ import importlib.resources as impr
 import os
 import re
 import shutil
+import subprocess as sp
 import tarfile
 import tempfile
+import venv
 from pathlib import Path
 
 import attr
 
 from kslurm.appconfig import get_config
-from kslurm.args.arg_types import FlagArg, PositionalArg, SubCommand, TailArg
+from kslurm.args.arg_types import FlagArg, PositionalArg, ShapeArg, SubCommand, TailArg
 from kslurm.args.command import command
 from kslurm.shell import Shell
 
@@ -62,13 +64,14 @@ def _load(args: _LoadModel):
         shell = Shell.get()
         shell.activate(pyload_venv_dir / name)
 
-    tmp = tempfile.mkdtemp(prefix="kslurm-", dir=slurm_tmpdir / "tmp")
+    (slurm_tmpdir / "tmp").mkdir(parents=True, exist_ok=True)
+    tmp = Path(tempfile.mkdtemp(prefix="kslurm-", dir=slurm_tmpdir / "tmp"))
     print(f"Unpacking venv {name}")
     with tarfile.open(Path(venv_cache, name).with_suffix(".tar.gz"), "r") as tar:
         tar.extractall(tmp)
 
     print("Updating paths")
-    with (pyload_venv_dir / name / "bin" / "activate").open("r") as f:
+    with (tmp / "bin" / "activate").open("r") as f:
         lines = f.read().splitlines()
         # \x22 and \x27 are " and ' char
         subbed = [
@@ -79,10 +82,10 @@ def _load(args: _LoadModel):
             )
             for line in lines
         ]
-    with (pyload_venv_dir / name / "bin" / "activate").open("w") as f:
+    with (tmp / "bin" / "activate").open("w") as f:
         f.write("\n".join(subbed))
 
-    for root, _, files in os.walk(pyload_venv_dir / name / "bin"):
+    for root, _, files in os.walk(tmp / "bin"):
         for file in files:
             path = Path(root, file)
             if path.is_symlink() or not os.access(path, os.X_OK):
@@ -100,8 +103,8 @@ def _load(args: _LoadModel):
             with path.open("w") as f:
                 f.write("\n".join(subbed))
 
-    pyload_venv_dir.mkdir(parents=True)
-    shutil.move(tmp, pyload_venv_dir / name)
+    pyload_venv_dir.mkdir(parents=True, exist_ok=True)
+    shutil.move(str(tmp), pyload_venv_dir / name)
     shell = Shell.get()
     shell.activate(pyload_venv_dir / name)
 
@@ -148,12 +151,47 @@ def _save(args: _SaveModel):
 
 
 @attr.frozen
+class _CreateModel:
+    name: PositionalArg[str] = PositionalArg("")
+    version: ShapeArg[str] = ShapeArg(
+        value="", match=lambda s: bool(re.match(r"^[23]\.\d{1,2}$", s))
+    )
+
+
+@command
+def _create(args: _CreateModel):
+    if os.environ.get("SLURM_TMPDIR"):
+        dir = tempfile.mkdtemp(
+            prefix="kslurm", dir=Path(os.environ["SLURM_TMPDIR"], "tmp")
+        )
+    else:
+        dir = tempfile.mkdtemp(prefix="kslurm-")
+
+    if args.version.value:
+        try:
+            sp.run(["command", "-v", "module"]).check_returncode()
+        except sp.CalledProcessError:
+            print("'module' command not present. Unable to load custom python versions")
+            return
+        sp.run(["module", "load", f"python/{args.version.value}"])
+
+    name = args.name.value if args.name.value else "venv"
+    venv.create(dir, symlinks=True, with_pip=True, prompt=name)
+    sp.run(
+        [os.path.join(dir, "bin", "python"), "-m", "pip", "install", "--upgrade", "pip"]
+    )
+    shell = Shell.get()
+    shell.activate(Path(dir))
+
+
+@attr.frozen
 class KpyModel:
     command: SubCommand = SubCommand(
         commands={
             "load": _load,
             "save": _save,
             "bash": _bash,
+            "create": _create,
         },
     )
 

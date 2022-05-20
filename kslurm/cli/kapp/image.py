cbe5e7acfe469d78b3133eee7fe21c8a4b52aa4a
Peter Van Dyken <pvandyk2@uwo.ca>
2022-05-19 23:49:15 -0400

    Implement image remove command

--- kslurm/cli/kapp/image.py
+++ kslurm/cli/kapp/image.py
@@ -1,20 +1,67 @@
 from __future__ import absolute_import
 
+import os
+
 import attrs
 
 from kslurm.args import Subcommand, command, positional, subcommand
+from kslurm.args.arg_types_cast import flag
 from kslurm.container import SingularityDir
 
 
 @command
 def _list():
+    """List all available containers"""
     for container in SingularityDir().iter_images():
         print(container)
 
 
-@command
-def _rm(img: str = positional()):
-    pass
+@command(inline=True)
+def _rm(
+    uri_or_alias: str = positional(),
+    purge: bool = flag(
+        ["-p", "--purge"], help="Remove all underlying data files (not yet implemented)"
+    ),
+):
+    """Remove a docker image uri.
+
+    By default, only the uri pointer is removed; underlying datafiles are kept. In a
+    future release, the purge flag will be available for deleting the underlying data.
+    """
+    singularity_dir = SingularityDir()
+    container = singularity_dir.find(uri_or_alias)
+    if container is None:
+        print(f"No image with identifier '{uri_or_alias}' found")
+        return 1
+
+    path = singularity_dir.uris / container.uri_path
+
+    if not path.is_symlink() and not purge:
+        print(
+            f"The uri {container.uri}' is directly attached to it's data and cannot "
+            "be removed without the --purge flag."
+        )
+        return 1
+
+    if path.is_symlink() and purge:
+        print("[INFO] The purge flag is not yet implemented in this context.")
+
+    for snakemake_alias in singularity_dir.snakemake.iterdir():
+        if snakemake_alias.is_symlink() and path.samefile(snakemake_alias):
+            snakemake_alias.unlink()
+            if path.is_symlink():
+                snakemake_alias.symlink_to(os.readlink(path))
+
+    for alias in singularity_dir.aliases.iterdir():
+        if alias.is_symlink() and path.samefile(alias):
+            alias.unlink()
+            if uri_or_alias != alias.name:
+                print(f"Removing alias '{alias.name}'")
+
+    if path.is_symlink():
+        path.unlink()
+    else:
+        os.remove(path)
 
 
 @attrs.frozen
